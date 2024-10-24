9478ff9b7cbd42d64f565ec37cb8bf680f376158
ndk98 <dev.ndk23071998@gmail.com>
2024-10-25 01:12:54 +0700

    Create homework for reactjs-24

---
+++ buoi24-reactjs/src/App.jsx
@@ -0,0 +1,18 @@
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <Nav />
+            <Header />
+            <Body />
+            <Footer />
+        </>
+    );
+}
+
+export default App;
